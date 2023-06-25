c04c54ede0355d54ffb29514ec40ba67d2c42f14
jakotide <111381232+jakotide@users.noreply.github.com>
2023-06-25 14:30:48 +0200

    Media queries, fixed close btn modal

--- script.js
+++ script.js
@@ -1,70 +1,84 @@
-
-(() =>{
- 
-  const openNavMenu = document.querySelector(".open-nav-menu"),
-  closeNavMenu = document.querySelector(".close-nav-menu"),
-  navMenu = document.querySelector(".nav-menu"),
-  menuOverlay = document.querySelector(".menu-overlay"),
-  mediaSize = 991;
+function navbar() {
+  const openNavMenu = document.querySelector(".open-nav-menu");
+  const closeNavMenu = document.querySelector(".close-nav-menu");
+  const navMenu = document.querySelector(".nav-menu");
+  const menuOverlay = document.querySelector(".menu-overlay");
+  const mediaSize = 991;
 
   openNavMenu.addEventListener("click", toggleNav);
   closeNavMenu.addEventListener("click", toggleNav);
-  // close the navMenu by clicking outside
   menuOverlay.addEventListener("click", toggleNav);
 
   function toggleNav() {
-  	navMenu.classList.toggle("open");
-  	menuOverlay.classList.toggle("active");
-  	document.body.classList.toggle("hidden-scrolling");
+    navMenu.classList.toggle("open");
+    menuOverlay.classList.toggle("active");
+    document.body.classList.toggle("hidden-scrolling");
   }
 
-  navMenu.addEventListener("click", (event) =>{
-      if(event.target.hasAttribute("data-toggle") && 
-      	window.innerWidth <= mediaSize){
-      	// prevent default anchor click behavior
-      	event.preventDefault();
-      	const menuItemHasChildren = event.target.parentElement;
-        // if menuItemHasChildren is already expanded, collapse it
-        if(menuItemHasChildren.classList.contains("active")){
-        	collapseSubMenu();
+  navMenu.addEventListener("click", (event) => {
+    if (
+      event.target.hasAttribute("data-toggle") &&
+      window.innerWidth <= mediaSize
+    ) {
+      event.preventDefault();
+      const menuItemHasChildren = event.target.parentElement;
+
+      if (menuItemHasChildren.classList.contains("active")) {
+        collapseSubMenu();
+      } else {
+        if (navMenu.querySelector(".menu-item-has-children.active")) {
+          collapseSubMenu();
         }
-        else{
-          // collapse existing expanded menuItemHasChildren
-          if(navMenu.querySelector(".menu-item-has-children.active")){
-        	collapseSubMenu();
-          }
-          // expand new menuItemHasChildren
-          menuItemHasChildren.classList.add("active");
-          const subMenu = menuItemHasChildren.querySelector(".sub-menu");
-          subMenu.style.maxHeight = subMenu.scrollHeight + "px";
-        }
+        menuItemHasChildren.classList.add("active");
+        const subMenu = menuItemHasChildren.querySelector(".sub-menu");
+        subMenu.style.maxHeight = subMenu.scrollHeight + "px";
       }
-  });
-  function collapseSubMenu(){
-  	navMenu.querySelector(".menu-item-has-children.active .sub-menu")
-  	.removeAttribute("style");
-  	navMenu.querySelector(".menu-item-has-children.active")
-  	.classList.remove("active");
-  }
-  function resizeFix(){
-  	 // if navMenu is open ,close it
-  	 if(navMenu.classList.contains("open")){
-  	 	toggleNav();
-  	 }
-  	 // if menuItemHasChildren is expanded , collapse it
-  	 if(navMenu.querySelector(".menu-item-has-children.active")){
-        	collapseSubMenu();
-     }
-  }
-
-  window.addEventListener("resize", function(){
-     if(this.innerWidth > mediaSize){
-     	resizeFix();
-     }
+    }
   });
 
-})();
+  function collapseSubMenu() {
+    navMenu.querySelector(".menu-item-has-children.active .sub-menu").removeAttribute("style");
+    navMenu.querySelector(".menu-item-has-children.active").classList.remove("active");
+  }
 
+  function resizeFix() {
+    if (navMenu.classList.contains("open")) {
+      toggleNav();
+    }
+    if (navMenu.querySelector(".menu-item-has-children.active")) {
+      collapseSubMenu();
+    }
+  }
+
+  window.addEventListener("resize", function () {
+    if (this.innerWidth > mediaSize) {
+      resizeFix();
+    }
+  });
+}
+
+navbar();
+
+// Link offset
+
+// Smooth scrolling behavior with an offset of 100 pixels
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    const offset = 90;
+
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - offset,
+        behavior: 'smooth'
+      });
+    }
+  });
+});
+
+// To top button
 
 const toTopBtn = document.querySelector(".to-top-btn");
 
@@ -78,13 +92,12 @@ function toTopButton(){
 
 toTopButton();
 
-
 // Modal
 
 // Get the modal element and the image elements
 const modal = document.getElementById("modal");
 const modalImage = document.getElementById("modal-image");
-const images = document.querySelectorAll(".page-container img");
+const images = document.querySelectorAll(".page-container img, .page-container-small img");
 
 // Attach a click event listener to each image
 images.forEach((image) => {
@@ -111,6 +124,7 @@ window.addEventListener("click", (event) => {
   }
 });
 
+// Close modal when clicking on modal
 modal.addEventListener("click", () => {
   modal.style.display = "none";
   document.body.style.overflow = "visible";
