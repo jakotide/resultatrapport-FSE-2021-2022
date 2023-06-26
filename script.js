159dc697aab62b12929e3938f110581dab31f4a8
jakotide <111381232+jakotide@users.noreply.github.com>
2023-06-26 15:36:10 +0200

    Fixing nav

--- script.js
+++ script.js
@@ -42,21 +42,45 @@
 // });
 
 
-/*=============== SHOW MENU ===============*/
-const showMenu = (toggleId, navId) =>{
-  const toggle = document.getElementById(toggleId),
-        nav = document.getElementById(navId)
+// /*=============== SHOW MENU ===============*/
+// const showMenu = (toggleId, navId) =>{
+//   const toggle = document.getElementById(toggleId),
+//         nav = document.getElementById(navId)
 
-  toggle.addEventListener('click', () =>{
-      // Add show-menu class to nav menu
-      nav.classList.toggle('show-menu')
+//   toggle.addEventListener('click', () =>{
+//       // Add show-menu class to nav menu
+//       nav.classList.toggle('show-menu')
 
-      // Add show-icon to show and hide the menu icon
-      toggle.classList.toggle('show-icon')
-  })
-}
+//       // Add show-icon to show and hide the menu icon
+//       toggle.classList.toggle('show-icon')
+//   })
+// }
 
-showMenu('nav-toggle','nav-menu')
+// showMenu('nav-toggle','nav-menu')
+
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId);
+  const nav = document.getElementById(navId);
+
+  toggle.addEventListener('click', () => {
+    // Toggle the show-menu class on the nav menu
+    if (nav.classList.contains('show-menu')) {
+      nav.classList.remove('show-menu');
+    } else {
+      nav.classList.add('show-menu');
+    }
+
+    // Toggle the show-icon class on the toggle button
+    if (toggle.classList.contains('show-icon')) {
+      toggle.classList.remove('show-icon');
+    } else {
+      toggle.classList.add('show-icon');
+    }
+  });
+};
+
+showMenu('nav-toggle', 'nav-menu');
+
 
 
 
