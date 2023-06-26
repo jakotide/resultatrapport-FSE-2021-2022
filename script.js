088eec4bd47317f36410686928bc54f4e0fb75d9
jakotide <111381232+jakotide@users.noreply.github.com>
2023-06-26 21:05:31 +0200

    Fixed navbar

--- script.js
+++ script.js
@@ -2,88 +2,6 @@
 //   const toggle = document.getElementById(toggleId);
 //   const nav = document.getElementById(navId);
 
-//   toggle.addEventListener('click', () => {
-//     nav.classList.toggle('show-menu');
-//     toggle.classList.toggle('show-icon');
-//   });
-
-//   // Get all dropdown items
-//   const dropdownItems = document.querySelectorAll('.dropdown__item');
-
-//   // Attach click event listener to each dropdown item
-//   dropdownItems.forEach((item) => {
-//     item.addEventListener('click', () => {
-//       // Toggle the active class on the clicked dropdown item
-//       item.classList.toggle('active');
-//     });
-//   });
-// };
-
-// showMenu('nav-toggle', 'nav-menu');
-
-// const showMenu = (toggleId, navId) => {
-//   const toggle = document.getElementById(toggleId);
-//   const nav = document.getElementById(navId);
-
-//   const closeMenu = () => {
-//     nav.classList.remove('show-menu');
-//     toggle.classList.remove('show-icon');
-//   };
-
-//   toggle.addEventListener('click', () => {
-//     nav.classList.toggle('show-menu');
-//     toggle.classList.toggle('show-icon');
-//   });
-
-//   // Get all dropdown items
-//   const dropdownItems = document.querySelectorAll('.dropdown__item');
-
-//   // Attach click event listener to each dropdown item
-//   dropdownItems.forEach((item) => {
-//     const link = item.querySelector('.nav__link');
-//     const submenu = item.querySelector('.dropdown__menu');
-
-//     link.addEventListener('click', () => {
-//       const isActive = item.classList.contains('active');
-
-//       // Close the submenu if it's already open
-//       if (isActive && submenu.classList.contains('show-submenu')) {
-//         submenu.classList.remove('show-submenu');
-//         item.classList.remove('active');
-//       } else {
-//         // Close other open submenus and remove active classes
-//         dropdownItems.forEach((otherItem) => {
-//           const otherSubmenu = otherItem.querySelector('.dropdown__menu');
-//           otherSubmenu.classList.remove('show-submenu');
-//           otherItem.classList.remove('active');
-//         });
-
-//         // Open the clicked submenu
-//         submenu.classList.add('show-submenu');
-//         item.classList.add('active');
-//       }
-//     });
-//   });
-
-//   // Close the menu when clicking outside
-//   document.addEventListener('click', (event) => {
-//     const target = event.target;
-//     if (!nav.contains(target) && !toggle.contains(target)) {
-//       closeMenu();
-//     }
-//   });
-// };
-
-// showMenu('nav-toggle', 'nav-menu');
-
-
-
-
-
-// const showMenu = (toggleId, navId) => {
-//   const toggle = document.getElementById(toggleId);
-//   const nav = document.getElementById(navId);
-
 //   const closeMenu = () => {
 //     nav.classList.remove('show-menu');
 //     toggle.classList.remove('show-icon');
@@ -132,6 +50,7 @@
 
 // showMenu('nav-toggle', 'nav-menu');
 
+
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId);
   const nav = document.getElementById(navId);
@@ -159,15 +78,15 @@ const showMenu = (toggleId, navId) => {
 
       dropdownItems.forEach((otherItem) => {
         const otherSubmenu = otherItem.querySelector('.dropdown__menu');
-        otherSubmenu.classList.remove('show-submenu');
+        otherSubmenu.style.maxHeight = '0';
         otherItem.classList.remove('active');
       });
 
       if (!isActive) {
-        submenu.classList.add('show-submenu');
+        submenu.style.maxHeight = submenu.scrollHeight + 'px';
         item.classList.add('active');
       } else {
-        submenu.classList.remove('show-submenu');
+        submenu.style.maxHeight = '0';
         item.classList.remove('active');
       }
     });
@@ -183,9 +102,6 @@ const showMenu = (toggleId, navId) => {
 };
 
 showMenu('nav-toggle', 'nav-menu');
-
-
-
 
 
 
@@ -243,39 +159,80 @@ toTopButton();
 
 // Modal
 
-// Get the modal element and the image elements
-const modal = document.getElementById("modal");
-const modalImage = document.getElementById("modal-image");
-const images = document.querySelectorAll(".page-container img, .page-container-small img");
+// // Get the modal element and the image elements
+// const modal = document.getElementById("modal");
+// const modalImage = document.getElementById("modal-image");
+// const images = document.querySelectorAll(".page-container img, .page-container-small img");
 
-// Attach a click event listener to each image
-images.forEach((image) => {
-  image.addEventListener("click", () => {
-    // Set the clicked image source as the modal image source
-    modalImage.src = image.src;
-    modal.style.display = "block";
-    document.body.style.overflow = "hidden";
+// // Attach a click event listener to each image
+// images.forEach((image) => {
+//   image.addEventListener("click", () => {
+//     // Set the clicked image source as the modal image source
+//     modalImage.src = image.src;
+//     modal.style.display = "block";
+//     document.body.style.overflow = "hidden";
+//   });
+// });
+
+// // Attach a click event listener to the close button
+// const closeBtn = document.querySelector(".close");
+// closeBtn.addEventListener("click", () => {
+//   document.body.style.overflow = "visible";
+//   modal.style.display = "none";
+// });
+
+// // Close the modal when clicking outside the modal content
+// window.addEventListener("click", (event) => {
+//   if (event.target === modal) {
+//     modal.style.display = "none";
+//     document.body.style.overflow = "visible";
+//   }
+// });
+
+// // Close modal when clicking on modal
+// modal.addEventListener("click", () => {
+//   modal.style.display = "none";
+//   document.body.style.overflow = "visible";
+// });
+
+// Check if the screen width is 450px or above
+if (window.matchMedia("(min-width: 450px)").matches) {
+  // Get the modal element and the image elements
+  const modal = document.getElementById("modal");
+  const modalImage = document.getElementById("modal-image");
+  const images = document.querySelectorAll(".page-container img, .page-container-small img");
+
+  // Attach a click event listener to each image
+  images.forEach((image) => {
+    image.addEventListener("click", () => {
+      // Set the clicked image source as the modal image source
+      modalImage.src = image.src;
+      modal.style.display = "block";
+      document.body.style.overflow = "hidden";
+    });
   });
-});
 
-// Attach a click event listener to the close button
-const closeBtn = document.querySelector(".close");
-closeBtn.addEventListener("click", () => {
-  document.body.style.overflow = "visible";
-  modal.style.display = "none";
-});
+  // Attach a click event listener to the close button
+  const closeBtn = document.querySelector(".close");
+  closeBtn.addEventListener("click", () => {
+    document.body.style.overflow = "visible";
+    modal.style.display = "none";
+  });
 
-// Close the modal when clicking outside the modal content
-window.addEventListener("click", (event) => {
-  if (event.target === modal) {
+  // Close the modal when clicking outside the modal content
+  window.addEventListener("click", (event) => {
+    // Check if the screen width is still 450px or above
+    if (window.matchMedia("(min-width: 450px)").matches && event.target === modal) {
+      modal.style.display = "none";
+      document.body.style.overflow = "visible";
+    }
+  });
+
+  // Close modal when clicking on modal
+  modal.addEventListener("click", () => {
     modal.style.display = "none";
     document.body.style.overflow = "visible";
-  }
-});
+  });
+}
 
-// Close modal when clicking on modal
-modal.addEventListener("click", () => {
-  modal.style.display = "none";
-  document.body.style.overflow = "visible";
-});
 
